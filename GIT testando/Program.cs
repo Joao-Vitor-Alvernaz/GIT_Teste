b957908b3c299fd73a3b53d8afbb46ee910a8ae6
João Vitor <jvitoralvernaz@outlook.com>
2023-05-30 15:27:12 -0400

    try e cath

--- GIT testando/Program.cs
+++ GIT testando/Program.cs
@@ -1,6 +1,13 @@
-﻿Console.Write("Informe o primeiro valor: ");
-int a  = int.Parse(Console.ReadLine());
-Console.Write("Informe o segundo valor: ");
-int b  = int.Parse(Console.ReadLine());
-int resultado = a / b;
-Console.WriteLine(resultado);
+﻿try
+{
+    Console.Write("Informe o primeiro valor: ");
+    int a = int.Parse(Console.ReadLine());
+    Console.Write("Informe o segundo valor: ");
+    int b = int.Parse(Console.ReadLine());
+    int resultado = a / b;
+    Console.WriteLine($"O resultado é {resultado}");
+}
+catch (Exception ex)
+{
+    Console.WriteLine("Divisão inacessível!");
+}
