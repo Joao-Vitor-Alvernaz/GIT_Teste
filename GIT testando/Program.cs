bf82203235adf6ae666cea637491ae9ef1a27a5d
João Vitor <jvitoralvernaz@outlook.com>
2023-05-30 14:52:48 -0400

    Meu primeiro commit

---
+++ GIT testando/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Write("Informe o primeiro valor: ");
+int a  = int.Parse(Console.ReadLine());
+Console.Write("Informe o segundo valor: ");
+int b  = int.Parse(Console.ReadLine());
+int resultado = a / b;
+Console.WriteLine(resultado);
